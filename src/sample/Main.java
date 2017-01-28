92b9e82cb3a4d7fc6034df816b214bcc43deeae0
dbrooksss <daniellebrooks07@yahoo.com>
2017-01-28 16:31:31 -0500

    first

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,18 +8,17 @@ import javafx.stage.Stage;
 
 import static com.sun.org.apache.xalan.internal.lib.ExsltDatetime.date;
 
-public class Main extends Application {
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("BakuSoft");
-        primaryStage.setScene(new Scene(root, 600, 300));
-        primaryStage.show();
-        System.out.println("Thank You For Using BakuSoft Copyright: 2017 "+ date());
-    }
+public class Main extends Application{
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+		primaryStage.setTitle("BakuSoft");
+		primaryStage.setScene(new Scene(root, 600, 300));
+		primaryStage.show();
+		System.out.println("Thank You For Using BakuSoft Copyright: 2017 " + date());
+	}
 
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+	public static void main(String[] args){
+		launch(args);
+	}
 }
